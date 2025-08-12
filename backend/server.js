64668f5affead73f119b3a6cf5ac4aa0532b761c
nadeemahmed12 <nadeemahmed9480@gmail.com>
2025-08-12 22:39:13 +0530

    server.js aur .env file update ki

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use("/api/quizzes", quizRoutes);
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => 
   console.log(`server is running on port ${PORT}`)
 );
